e819893147e47528378c318869be1fb61e2e0069
dmtweedy <damon.tweedy@yahoo.com>
2023-08-23 10:54:00 -0500

    added controllers to file structure, added models, added api routes

---
+++ models/Thought.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const thoughtSchema = new mongoose.Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    maxlength: 280,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => dateFormat(timestamp),
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  reactions: [ReactionSchema],
+});
+
+// Virtual to get reactionCount
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
+
+const Thought = mongoose.model('Thought', thoughtSchema);
+
+module.exports = Thought;
